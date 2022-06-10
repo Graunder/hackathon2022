0e2bb908150b1b377191c8ebf18f59fedb117ff3
unknown <graunder@gmail.com>
2022-06-11 02:53:29 +0300

    weapon upgrade and mixing colors

--- Assets/_Hackathon/Scripts/Projectile.cs
+++ Assets/_Hackathon/Scripts/Projectile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorCanister : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
-    public Color canisterColor;
+    public string projectileColorName;
 }
--- Assets/_Hackathon/Scripts/Weapon.cs
+++ Assets/_Hackathon/Scripts/Weapon.cs
@@ -10,18 +10,67 @@ public class Weapon : MonoBehaviour
     private SnapZone snapR;
     [SerializeField]
     private SnapZone snapL;
+    [SerializeField]
+    private GameObject projectile;
+    private Color projectileColor;
+    private Color colorWhite = Color.white;
+
+    public Material[] colorCombinations;
 
     public void Shoot(){
-        GetColor();
-        Debug.Log("Shoot");
+        GetNewProjectile();
+        // Debug.Log($"Shoot - Color: {snapR.HeldItem.GetComponent<ColorCanister>().canisterColor}");
+        projectile.GetComponentInChildren<MeshRenderer>().material.color = projectileColor;
     }
 
-    private void GetColor(){
+    private void GetNewProjectile(){
 
         if(snapL.HeldItem != null && snapR.HeldItem != null){
-            Debug.Log($"Colors - snapL: {snapL.HeldItem.GetComponent<ColorCanister>().colorName}, snapR: {snapR.HeldItem.GetComponent<ColorCanister>().colorName}");
+            ChangeProjectileColor(
+                snapL.HeldItem.GetComponent<ColorCanister>().canisterColor,
+                snapR.HeldItem.GetComponent<ColorCanister>().canisterColor
+            );
+            // Debug.Log($"Colors - snapL: {snapL.HeldItem.GetComponent<ColorCanister>().canisterColor}, snapR: {snapR.HeldItem.GetComponent<ColorCanister>().canisterColor}");
+        }else if(snapL.HeldItem != null){
+            ChangeProjectileColor(
+                snapL.HeldItem.GetComponent<ColorCanister>().canisterColor
+            );
+            // Debug.Log($"Colors - snapL: {snapL.HeldItem.GetComponent<ColorCanister>().canisterColor}");
+        }else if(snapR.HeldItem != null){
+            ChangeProjectileColor(
+                snapR.HeldItem.GetComponent<ColorCanister>().canisterColor
+            );
+            // Debug.Log($"Colors - snapR: {snapR.HeldItem.GetComponent<ColorCanister>().canisterColor}");
+        }else{
+            ChangeProjectileColor();
+            
         }
 
+    }
+
+    private void ChangeProjectileColor(Color? color1 = null, Color? color2 = null){
+        if(color1 != null && color2 != null){
+            projectileColor = Color.Lerp((Color)color1, (Color)color2, 0.5f);
+        }else if(color1 != null){
+            projectileColor = (Color)color1;
+        }else{
+            projectileColor = Color.white;
+            Debug.Log("Colors - none = white");
+        }
+
+        // switch(color1, color2){
+        //     // case ("", ""):
+        //     //     break;
+        //     case ("Blue", ""):
+        //         break;
+        //     case ("Red", ""):
+        //         break;
+        //     case ("Yellow", ""):
+        //         break;
+        //     default: 
+        //         projectileMaterial = colorCombinations[0];
+        //         break;
+        // }
     }
 
 }
