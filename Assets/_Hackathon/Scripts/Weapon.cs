c0ff97bdaf8cba9f8f5f1e9f35aab0c6284362f8
unknown <graunder@gmail.com>
2022-06-11 00:59:39 +0300

    string for canister color

--- Assets/_Hackathon/Scripts/Weapon.cs
+++ Assets/_Hackathon/Scripts/Weapon.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using BNG;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     
+    [SerializeField]
+    private SnapZone snapR;
+    [SerializeField]
+    private SnapZone snapL;
+
     public void Shoot(){
+        GetColor();
         Debug.Log("Shoot");
+    }
+
+    private void GetColor(){
+
+        if(snapL.HeldItem != null && snapR.HeldItem != null){
+            Debug.Log($"Colors - snapL: {snapL.HeldItem.GetComponent<ColorCanister>().colorName}, snapR: {snapR.HeldItem.GetComponent<ColorCanister>().colorName}");
+        }
+
     }
 
 }
